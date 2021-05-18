7f8fd5c8acd588b503146f7af7626e361efd17de
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-18 10:10:25 +0530

    allowed to choose option

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,7 @@
 package workshop1;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	
 	public static char[] createBoard() {
@@ -10,10 +12,30 @@ public class TicTacToeGame {
 		}
 		return board;
 	}
+		public static void chooseOption () {
+		
+			char computersSymbol = 0;
+			
+			Scanner sc = new Scanner(System.in);
+			System.out.print("give the  option 'o' or 'x' : ");
+			char option = sc.next().charAt(0);
+			
+		
+			if ( option=='o') {
+				computersSymbol='x';
+			} else if (option=='x'){
+				computersSymbol='0';
+			} else {
+				System.out.print("invalied option: ");
+			}
+			System.out.println("Players option : "+ option+"\nand computers option is : "+computersSymbol);
+	
+	}
 	
 	public static void main(String[] args) {	
 	System.out.println("Welcome to the TicTacGame");
 	char[] board = createBoard();
+	chooseOption();
 
 	}
 
