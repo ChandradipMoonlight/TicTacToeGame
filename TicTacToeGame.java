2ecaf6dc938b73494b21664996ae9713ff1dcf44
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-18 09:04:51 +0530

    board created

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,8 +1,19 @@
 package workshop1;
 
 public class TicTacToeGame {
+	
+	public static char[] createBoard() {
+		char[] board = new char[10];
+	
+		for (int i=1; i<10; i++) {
+			board[i] = ' ';
+		}
+		return board;
+	}
+	
 	public static void main(String[] args) {	
 	System.out.println("Welcome to the TicTacGame");
+	char[] board = createBoard();
 
 	}
 
